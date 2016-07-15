cfe012cea18c8ffa804063ce893b7c100391294d
Darius Capizzi <DariusCapizzi@gmail.com>
2016-07-15 16:09:48 -0700

    beginning of refactor Direction into Route and Step

--- app/src/main/java/com/example/darius/sharelocation/models/Step.java
+++ app/src/main/java/com/example/darius/sharelocation/models/Step.java
@@ -2,8 +2,49 @@ package com.example.darius.sharelocation.models;
 
 import android.os.Parcelable;
 
+import java.util.ArrayList;
+
 /**
  * Created by Guest on 7/15/16.
  */
-public class Step implements Parcelable {
+public class Step{
+
+    ArrayList<Step> stepArray =  new ArrayList<>();
+
+    private Direction parentDirection;
+
+    public Step(String distance, String duration, String htmlInstruction){
+        this.mDistance = distance;
+        this.mDuration = duration;
+        this.mHtmlInstruction = htmlInstruction;
+
+        stepArray.add(this);
+    }
+
+    public Direction getParentDirection() {
+        return parentDirection;
+    }
+
+    public void setParentDirection(Direction parentDirection) {
+        this.parentDirection = parentDirection;
+    }
+
+
+    public String getHtmlInstruction() {
+        return mHtmlInstruction;
+    }
+
+    public String getDuration() {
+        return mDuration;
+    }
+
+    public String getDistance() {
+        return mDistance;
+    }
+
+    private final String mHtmlInstruction;
+    private final String mDuration;
+    private final String mDistance;
+
+
 }
